ed770a864dc6b3dfbf4425c7fc43ba127ed89a54
CarloYT2011 <167158733+CarloYT2011@users.noreply.github.com>
2026-08-13 23:09:28 +0000

    Returned

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@
     y las muestra usando la API de notificaciones del sistema operativo.
 */
 
-const CACHE_NAME = 'digital-minds-v14';
+const CACHE_NAME = 'digital-minds-v15';
 
 const APP_SHELL = [
   './',
