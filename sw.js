abb805fab59844776528724bd725a571ae37ab73
CarloYT2011 <cg4163422@gmail.com>
2026-08-16 18:22:43 -0500

    Voz

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@
     y las muestra usando la API de notificaciones del sistema operativo.
 */
 
-const CACHE_NAME = 'digital-minds-v16';
+const CACHE_NAME = 'digital-minds-v19';
 
 const APP_SHELL = [
   './',
